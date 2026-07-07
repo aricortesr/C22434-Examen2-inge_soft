0de4dd93768c6c98a4d51036d1c2fd8217b01500
Arianna Cortes <ariannacortesr@gmail.com>
2026-07-06 19:12:58 -0600

    Created separate models for objects to remove them from the controller

---
+++ ExamTwo/Models/Payment.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ExamTwo.Models;
+
+public class Payment
+{
+    public int TotalAmount { get; set; }
+    public List<int> Coins { get; set; }
+    public List<int> Bills { get; set; }
+}
