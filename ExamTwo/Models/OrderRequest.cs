e275f85eb814e3363742be80ab2e6c43fe8b164a
Arianna Cortes <ariannacortesr@gmail.com>
2026-07-06 20:06:17 -0600

    Fixed order request model

--- ExamTwo/Models/OrderRequest.cs
+++ ExamTwo/Models/OrderRequest.cs
@@ -4,6 +4,6 @@ namespace ExamTwo.Models;
 
 public class OrderRequest
 {
-    public Dictionary<string, int> Order { get; set; }
-    public Payment Payment { get; set; }
+    public Dictionary<string, int> Order { get; set; } = new();
+    public Payment Payment { get; set; } = new();
 }
