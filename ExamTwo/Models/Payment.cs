99d7f9633bfb96756abf278b6a07dac4a9d032e1
Arianna Cortes <ariannacortesr@gmail.com>
2026-07-06 20:16:58 -0600

    Only used

--- ExamTwo/Models/Payment.cs
+++ ExamTwo/Models/Payment.cs
@@ -5,8 +5,4 @@ namespace ExamTwo.Models;
 public class Payment
 {
     public int TotalAmount { get; set; }
-
-    public List<int>? Coins { get; set; } 
-
-    public List<int>? Bills { get; set; }
 }
