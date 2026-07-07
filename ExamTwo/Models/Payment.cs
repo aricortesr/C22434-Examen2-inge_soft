ef96bb614c72eb025b87c116664a23050df2e490
Arianna Cortes <ariannacortesr@gmail.com>
2026-07-06 20:05:25 -0600

    Fixed payment model regarding nullable

--- ExamTwo/Models/Payment.cs
+++ ExamTwo/Models/Payment.cs
@@ -5,6 +5,8 @@ namespace ExamTwo.Models;
 public class Payment
 {
     public int TotalAmount { get; set; }
-    public List<int> Coins { get; set; }
-    public List<int> Bills { get; set; }
+
+    public List<int>? Coins { get; set; } 
+
+    public List<int>? Bills { get; set; }
 }
